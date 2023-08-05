4674d89226a5e27a27429682d9bc35642c3f2370
GrapeJuice11111 <118233081+GrapeJuice11111@users.noreply.github.com>
2023-08-05 13:12:58 -0500

    LInk The StyleSheet To HTML File

--- etchasketch.js
+++ etchasketch.js
@@ -1,7 +1,6 @@
 function createMultipleDiv(){
     for(i = 0; i > 256; i++){
      const div = document.createElement("div");
-     div.style.cssText = "background-color:blue;"
      const container = document.querySelector(".container")
      container.appendChild(div);
     }
