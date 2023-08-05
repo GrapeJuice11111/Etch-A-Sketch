2c6464fdb35c7daa13f294d0788f88354cd2f18a
GrapeJuice11111 <118233081+GrapeJuice11111@users.noreply.github.com>
2023-08-05 13:09:47 -0500

    Append The Divs To The Container Div

--- etchasketch.js
+++ etchasketch.js
@@ -1,5 +1,10 @@
 function createMultipleDiv(){
     for(i = 0; i > 256; i++){
-        const div = document.createElement("div");
+     const div = document.createElement("div");
+     div.style.cssText = "background-color:blue;"
+     const container = document.querySelector(".container")
+     container.appendChild(div);
     }
+    
 }
+createMultipleDiv();
