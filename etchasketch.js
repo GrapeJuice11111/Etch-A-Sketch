7d28e0f304a5393996e80aa0fbf0f650934a5fc6
GrapeJuice11111 <118233081+GrapeJuice11111@users.noreply.github.com>
2023-08-05 23:34:00 -0500

    Create A Function Which Deletes Sketch And Creates A New One
    According to user prompt makes a new sketchpad to use after clicking reset

--- etchasketch.js
+++ etchasketch.js
@@ -6,11 +6,30 @@ function createMultipleDiv(){
      divs.setAttribute("class","multiple")
     }};
     createMultipleDiv();
-
+    const container = document.querySelector(".container")
     const divs = document.querySelectorAll(".multiple")
     divs.forEach(div => div.addEventListener("mouseover", function(e){
         div.classList.add("hovering");
     }));
 
-
-    
+   const btn = document.querySelector(".reset")
+   btn.addEventListener("click", function(e){
+    const input = prompt("How many number of grids do you want?");
+    const collection = document.querySelectorAll('.multiple');
+    for (let i = 0; i < collection.length; i++) {
+    const elem = collection[i];
+    elem.parentNode.removeChild(elem);
+    }
+    const num = parseInt(input);
+    function createMultipleDiv1(){
+        for(i = 0; i < num*num; i++){
+         const divs = document.createElement("div");
+         const container = document.querySelector(".container")
+         container.appendChild(divs);
+         divs.setAttribute("class","multiple")
+        }};
+        createMultipleDiv1();
+        const divs = document.querySelectorAll(".multiple")
+    divs.forEach(div => div.addEventListener("mouseover", function(e){
+        div.classList.add("hovering");
+    }));})
