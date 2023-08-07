10d902a1ac068590c4afd5dca3507bd8625ad5dd
GrapeJuice11111 <118233081+GrapeJuice11111@users.noreply.github.com>
2023-08-06 19:34:18 -0500

    Refactor The Code
    Refactoring the code to b compact

--- etchasketch.js
+++ etchasketch.js
@@ -15,13 +15,13 @@ function createMultipleDiv(){
    const btn = document.querySelector(".reset")
    btn.addEventListener("click", function(e){
     const input = prompt("How many number of grids do you want?");
-    const collection = document.querySelectorAll('.multiple');
+    const num = parseInt(input);
+    function createMultipleDiv1(){
+        const collection = document.querySelectorAll('.multiple');
     for (let i = 0; i < collection.length; i++) {
     const elem = collection[i];
     elem.parentNode.removeChild(elem);
     }
-    const num = parseInt(input);
-    function createMultipleDiv1(){
         for(i = 0; i < num*num; i++){
          const divs = document.createElement("div");
          const container = document.querySelector(".container")
